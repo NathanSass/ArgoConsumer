b80e16dba2d97a9facc86a989a4798c68047c209
Nathan Sass <nsass711@gmail.com>
2016-05-27 16:09:09 -0400

    Initial Commit

---
+++ argoConsumer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  argoConsumer
+//
+//  Created by Nathan Sass on 5/27/16.
+//  Copyright © 2016 RideArgo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
